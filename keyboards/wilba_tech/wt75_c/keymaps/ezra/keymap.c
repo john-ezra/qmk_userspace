78913203061d16d9a55bef402fd1b5776a9bcd53
John Ezra <john@johnezra.com>
2026-05-28 09:46:44 -0700

    Update Ezra WT65 and WT75 keymaps

--- keyboards/wilba_tech/wt75_c/keymaps/ezra/keymap.c
+++ keyboards/wilba_tech/wt75_c/keymaps/ezra/keymap.c
@@ -7,12 +7,12 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ansi_rwkl_split_bs(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  MO(_FN),
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_CAPS, _______, KC_DEL,
         KC_TAB,  KC_Z,    KC_R,    KC_L,    KC_D,    KC_W,    KC_Y,    KC_P,    KC_U,    KC_X,    KC_Q,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
         KC_BSPC, KC_H,    KC_N,    KC_T,    KC_S,    KC_C,    KC_B,    KC_I,    KC_E,    KC_O,    KC_A,    KC_QUOT, KC_ENT,                    KC_PGDN,
         KC_LSFT, KC_K,    KC_V,    KC_M,    KC_F,    KC_G,    KC_J,    KC_SCLN, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   KC_UP,
-        KC_RALT, KC_LCTL, KC_LGUI,                            KC_SPC,                             KC_RGUI, MO(_FN),          KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RGUI, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_FN] = LAYOUT_ansi_rwkl_split_bs(
