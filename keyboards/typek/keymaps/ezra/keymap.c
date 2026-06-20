8a1a7ad6e7cbaaf2988f03e3e1f485e53f4a2f88
John Ezra <john@johnezra.com>
2026-06-19 23:49:05 -0700

    Add GOK TypeK Colemak keymap

---
+++ keyboards/typek/keymaps/ezra/keymap.c
@@ -0,0 +1,41 @@
+#include "keycodes.h"
+#include QMK_KEYBOARD_H
+
+enum layers {
+    _COLEMAK,
+    _FN,
+};
+
+// Keep the keymap in visual TypeK order while feeding QMK's matrix-ordered LAYOUT_all.
+#define LAYOUT_typek_visual( \
+    k00, k01, k02, k03, k04, k05, k06, k46, k07, k08, k09, k0A, k0B, k0C, k0D, \
+    k10, k11, k12, k13, k14, k15, k16,      k17, k18, k19, k1A, k1B, k1C, k1D, k4A, \
+    k20, k21, k22, k23, k24, k25, k26,      k27, k28, k29, k2A, k2B, k2C,      k4B, \
+    k30,      k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C, k3D, \
+         k41, k42,      k44, k45,                k47, k48, k49,                k4D  \
+) \
+    LAYOUT_all( \
+        k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D, \
+        k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D, \
+        k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, k2C, KC_NO, \
+        k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C, k3D, \
+             k41, k42,      k44, k45, k46, k47, k48, k49, k4A, k4B, KC_NO, k4D \
+    )
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_COLEMAK] = LAYOUT_typek_visual(
+        KC_ESC,  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+        KC_DEL,  KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_PGUP, KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,             KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, KC_ENT,
+        KC_PGDN, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
+                 KC_LCTL,          MO(_FN), KC_SPC,  KC_LGUI,                   KC_RGUI, KC_SPC,  KC_RALT,                            KC_RCTL
+    ),
+
+    [_FN] = LAYOUT_typek_visual(
+        QK_BOOT, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,
+        _______, _______, KC_HOME, _______, KC_END,  _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,          _______, _______, _______, _______, _______, _______,          _______,
+        _______,          _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
+                 _______, _______,          _______, _______,                   _______, _______, _______,                            _______
+    ),
+};
