2f3874dc1e8d44b4fb767c677022ab7a8665f54f
John Ezra <john@johnezra.com>
2026-06-27 12:48:24 -0700

    type k update

--- keyboards/typek/keymaps/ezra/keymap.c
+++ keyboards/typek/keymaps/ezra/keymap.c
@@ -1,4 +1,5 @@
 #include "keycodes.h"
+#include "quantum_keycodes.h"
 #include QMK_KEYBOARD_H
 
 enum layers {
@@ -26,9 +27,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT_typek_visual(
         KC_ESC,  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL,
         KC_PGUP, KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,             KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSPC,
-        KC_PGDN, KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,             KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,          KC_ENT,
-        MO(_FN), KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RCTL,
-                 KC_LALT,          MO(_FN), KC_SPC,  KC_LGUI,                   KC_RGUI, KC_SPC,  MO(_FN),                            KC_RALT
+        KC_PGDN, MO(_FN), KC_A,    KC_R,    KC_S,    KC_T,    KC_D,             KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,          KC_ENT,
+        KC_END,  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_FN),
+                 KC_LCTL,          KC_LALT, KC_SPC,  KC_LGUI,                   KC_RGUI, KC_SPC,  MO(_FN),                            KC_RCTL
     ),
 
     [_FN] = LAYOUT_typek_visual(
