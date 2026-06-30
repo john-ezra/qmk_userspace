057bda74176d293cd28630d6979ab85cea622279
John Ezra <john@johnezra.com>
2026-06-29 21:23:16 -0700

    add iron 160 support

---
+++ keyboards/smithrune/iron160/iron160_s/keymaps/ezra/keymap.c
@@ -0,0 +1,26 @@
+#include "keycodes.h"
+#include "quantum_keycodes.h"
+#include QMK_KEYBOARD_H
+
+enum layers {
+    _COLEMAK,
+    _FN,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_COLEMAK] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
+        QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,          KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_FN),
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                            KC_RALT, KC_RGUI, KC_RCTL
+    ),
+
+    [_FN] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
+        QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_DEL,
+        _______, BL_TOGG, BL_STEP, BL_UP,   BL_DOWN, BL_BRTG, _______, _______, _______, _______, _______, _______, _______, KC_INS,
+        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______
+    ),
+};
