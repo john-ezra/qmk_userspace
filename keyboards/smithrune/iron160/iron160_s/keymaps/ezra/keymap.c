ad00de68e5717c1fe440efac776aed90a60e8d25
John Ezra <john@johnezra.com>
2026-06-30 09:48:27 -0700

    add layer tap

--- keyboards/smithrune/iron160/iron160_s/keymaps/ezra/keymap.c
+++ keyboards/smithrune/iron160/iron160_s/keymaps/ezra/keymap.c
@@ -1,6 +1,8 @@
 #include "keycodes.h"
-#include "quantum_keycodes.h"
 #include QMK_KEYBOARD_H
+
+#define FN_TAB LT(_FN, KC_TAB)
+#define FN_ENT LT(_FN, KC_ENT)
 
 enum layers {
     _COLEMAK,
@@ -9,9 +11,9 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
-        QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, _______,
-        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
-        KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,          KC_ENT,
+        QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, _______,
+        KC_BSPC, KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSPC,
+        FN_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,          FN_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          MO(_FN),
         KC_LCTL, KC_LGUI, MO(_FN),                            KC_SPC,                                      KC_RGUI, KC_RALT, KC_RCTL
     ),
