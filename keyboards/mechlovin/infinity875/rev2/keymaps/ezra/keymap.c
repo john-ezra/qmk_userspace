9dc1890f8ac261c6665222919c7ffe749a875be3
John Ezra <john@johnezra.com>
2026-07-03 01:29:46 -0700

    add f1-8x 722 support

---
+++ keyboards/mechlovin/infinity875/rev2/keymaps/ezra/keymap.c
@@ -0,0 +1,45 @@
+/* Copyright 2021 Mechlovin' Studio
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "keycodes.h"
+#include QMK_KEYBOARD_H
+
+enum layers {
+    _COLEMAK,
+    _FN,
+};
+
+#define SUP_TAB LGUI_T(KC_TAB)
+#define SUP_ENT RGUI_T(KC_ENT)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_COLEMAK] = LAYOUT_all(
+        KC_ESC,    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,             KC_PSCR, KC_SCRL, KC_PAUS,
+        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, _______,   KC_INS,  KC_HOME, KC_PGUP,
+        KC_ESC,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSPC,            KC_DEL,  KC_END,  KC_PGDN,
+        SUP_TAB,   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,          SUP_ENT,
+        KC_LSFT,   _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RALT,                     KC_UP,
+        KC_LCTL,   _______, MO(_FN),                            KC_SPC,                             _______, MO(_FN), _______, KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+
+    [_FN] = LAYOUT_all(
+        QK_BOOT,   _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______,
+        _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______,
+        _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______,
+        _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                     _______,
+        _______,   _______, _______,                            _______,                            _______, _______, _______, _______,            _______, _______, _______
+    )
+};
