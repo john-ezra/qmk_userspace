e83e0ec92ce10575b4f704f1b0d121e615355a05
John Ezra <john@johnezra.com>
2026-07-04 01:26:53 -0700

    F1-8x 722 keymap update

--- keyboards/mechlovin/infinity875/rev2/keymaps/ezra/config.h
+++ keyboards/mechlovin/infinity875/rev2/keymaps/ezra/config.h
@@ -1,3 +1,4 @@
 #pragma once
 
 #define TAPPING_TERM 100
+
--- keyboards/mechlovin/infinity875/rev2/keymaps/ezra/keymap.c
+++ keyboards/mechlovin/infinity875/rev2/keymaps/ezra/keymap.c
@@ -21,15 +21,15 @@ enum layers {
     _FN,
 };
 
-#define SUP_TAB LGUI_T(KC_TAB)
+#define SUP_ESC LGUI_T(KC_ESC)
 #define SUP_ENT RGUI_T(KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT_all(
         KC_ESC,    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,             KC_PSCR, KC_SCRL, KC_PAUS,
         KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, _______,   KC_INS,  KC_HOME, KC_PGUP,
-        KC_ESC,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSPC,            KC_DEL,  KC_END,  KC_PGDN,
-        SUP_TAB,   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,          SUP_ENT,
+        KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSPC,            KC_DEL,  KC_END,  KC_PGDN,
+        SUP_ESC,   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,          SUP_ENT,
         KC_LSFT,   _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RALT,                     KC_UP,
         KC_LCTL,   _______, MO(_FN),                            KC_SPC,                             _______, MO(_FN), _______, KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT
     ),
