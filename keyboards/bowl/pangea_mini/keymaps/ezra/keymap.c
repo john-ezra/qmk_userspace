5ac73a2d59c6abbf0962edcb8ba9ffedccb2cb3b
John Ezra <john@johnezra.com>
2026-07-05 21:39:21 -0700

    Add symbols to function layer

--- keyboards/bowl/pangea_mini/keymaps/ezra/keymap.c
+++ keyboards/bowl/pangea_mini/keymaps/ezra/keymap.c
@@ -20,9 +20,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_FN] = LAYOUT_60_ansi_split_right_shift(
         QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,
-        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,          _______,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, KC_TILD, KC_LCBR, KC_RCBR, _______, _______, _______, _______, KC_INS,
+        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_MINS, KC_LPRN, KC_RPRN, _______, _______, _______,          _______,
+        _______,          _______, _______, _______, _______, _______, KC_UNDS, KC_LBRC, KC_RBRC, _______, _______, _______, _______,
         _______, _______, _______,                            _______,                                     _______, _______, _______
     ),
 };
