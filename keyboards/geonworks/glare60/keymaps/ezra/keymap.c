19b6712d44ed15baa72940496b12f544ec5f5ebc
John Ezra <john@null.ltd>
2026-08-07 18:18:40 -0700

    Update Glare660 keymap

--- keyboards/geonworks/glare60/keymaps/ezra/keymap.c
+++ keyboards/geonworks/glare60/keymaps/ezra/keymap.c
@@ -16,8 +16,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSPC,
         SUP_ESC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, KC_NUHS, SUP_ENT, SUP_ENT,
-        KC_LSFT, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_FN),
-        KC_LCTL, KC_LALT,                            KC_SPC,                                      KC_RALT, KC_RCTL
+        KC_LSFT, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RALT,
+        KC_LCTL, MO(_FN),                            KC_SPC,                                      MO(_FN), KC_RCTL
     ),
 
     [_FN] = LAYOUT_all(
