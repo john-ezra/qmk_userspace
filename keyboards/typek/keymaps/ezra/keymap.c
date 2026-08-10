b0c8878971bd1ec389afaa53bec048eeb00375b1
John Ezra <john@null.ltd>
2026-08-10 00:10:24 -0700

    Update Type K keymap

--- keyboards/typek/keymaps/ezra/keymap.c
+++ keyboards/typek/keymaps/ezra/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT_typek_visual(
         KC_ESC,  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL,
         KC_PGUP, KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,             KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSPC,
-        KC_PGDN, KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,             KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,          KC_ENT,
+        KC_PGDN, SUP_ESC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,             KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,          SUP_ENT,
         TG_ADJ,  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RALT,
                  KC_LCTL,          MO(_FN), KC_SPC,  KC_LGUI,                   KC_RGUI, KC_SPC,  MO(_FN),                            KC_RCTL
     ),
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,          UG_TOGG, UG_HUEU, UG_SATU, UG_VALU, UG_SPDU, UG_NEXT, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,          _______, UG_HUED, UG_SATD, UG_VALD, UG_SPDD, UG_PREV,          _______,
-        TG_ADJ,  _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,  _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
                  _______, _______,          _______, _______,                   _______, _______, _______,                            _______
     ),
 };
