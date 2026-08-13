d4456c3d0f5dee89d36afaca3b041d237bffa31e
John Ezra <john@null.ltd>
2026-08-13 11:35:58 -0700

    Update Glare60 keymap

--- keyboards/geonworks/glare60/keymaps/ezra/keymap.c
+++ keyboards/geonworks/glare60/keymaps/ezra/keymap.c
@@ -10,14 +10,16 @@ enum layers {
 
 #define SUP_ESC LGUI_T(KC_ESC)
 #define SUP_ENT RGUI_T(KC_ENT)
+#define FN_TAB LT(_FN, KC_TAB)
+#define FN_BSLS LT(_FN, KC_BSLS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT_all(
-        QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, KC_BSLS,
-        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSPC,
+        QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, KC_BSPC,
+        FN_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, FN_BSLS,
         SUP_ESC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, KC_NUHS, SUP_ENT, SUP_ENT,
         KC_LSFT, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RALT,
-        KC_LCTL, MO(_FN),                            KC_SPC,                                      MO(_FN), KC_RCTL
+        KC_LCTL, KC_LALT,                            KC_SPC,                                      KC_RALT, KC_RCTL
     ),
 
     [_FN] = LAYOUT_all(
