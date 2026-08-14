51ac9cc9f7f917ef022f5699394ea44fe0dc7ca8
John Ezra <john@null.ltd>
2026-08-13 18:20:48 -0700

    Update Vector and Glare60 keymap

--- keyboards/cannonkeys/vector/keymaps/ezra/keymap.c
+++ keyboards/cannonkeys/vector/keymaps/ezra/keymap.c
@@ -14,15 +14,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, _______,
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSPC,
         SUP_ESC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,          SUP_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_RALT,
-        KC_LCTL, _______, MO(_FN),                            KC_SPC,                                      MO(_FN), _______, KC_RCTL
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          MO(_FN),
+        KC_LCTL, _______, KC_LALT,                            KC_SPC,                                      KC_RALT, _______, KC_RCTL
     ),
 
     [_FN] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
         QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______,
-        _______, _______, _______, _______, _______, _______, KC_LEFT, KC_RGHT, _______, _______, _______, _______, _______, KC_INS,
-        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_UP,   _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, KC_DOWN, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,
+        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______,                            KC_LGUI,                                     _______, _______, _______
     ),
 };
