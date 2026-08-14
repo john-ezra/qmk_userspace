8be96725a6ef9a9941430352332ccab690a0b6e9
John Ezra <john@null.ltd>
2026-08-13 22:34:58 -0700

    Update 7v keymap

--- keyboards/wilba_tech/wt75_c/keymaps/ezra/keymap.c
+++ keyboards/wilba_tech/wt75_c/keymaps/ezra/keymap.c
@@ -16,15 +16,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSPC,          KC_PGUP,
         SUP_ESC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, SUP_ENT,                   KC_PGDN,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   KC_UP,
-        KC_LCTL, KC_LALT, MO(_FN),                            KC_SPC,                             KC_RCTL, MO(_FN),          KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LALT, MO(_FN),                            KC_SPC,                             MO(_FN), KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_FN] = LAYOUT_ansi_rwkl_split_bs(
         QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,
-        _______, _______, _______, _______, _______, _______, KC_LEFT, KC_RGHT, _______, _______, _______, _______, _______, _______,          KC_HOME,
-        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_UP,   _______, _______, _______, _______, _______, _______,                   KC_END,
-        _______, _______, _______, _______, _______, _______, KC_DOWN, _______, _______, _______, _______, _______,                   _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_HOME,
+        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,                   KC_END,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
         _______, _______, _______,                            KC_LGUI,                            _______, _______,          _______, _______, _______
     ),
 };
