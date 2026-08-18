1302fb2b0464b1cc45ef3a3b6858f2175a988196
John Ezra <john@null.ltd>
2026-08-18 14:16:46 -0700

    Update Lena Mini keymap

--- keyboards/kopibeng/tgr_lena/keymaps/ezra/keymap.c
+++ keyboards/kopibeng/tgr_lena/keymaps/ezra/keymap.c
@@ -20,9 +20,9 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT_all(
-        KC_F1,   QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, _______, _______, _______,
-        KC_F2,   FN_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC,          FN_BSLS, _______, _______, _______,
-        KC_F3,   SUP_ESC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,                   SUP_ENT, _______, _______, _______,
+        KC_DEL,  QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, _______, _______, _______,
+        KC_PGUP, FN_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC,          FN_BSLS, _______, _______, _______,
+        KC_PGDN, SUP_ESC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,                   SUP_ENT, _______, _______, _______,
                  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          MO(_FN), _______, _______, _______,
                  KC_LCTL,          KC_LALT,          KC_SPC,  KC_LGUI,          KC_SPC,           KC_RALT,                   KC_RGUI,          KC_RCTL, _______, _______, _______
     ),
