118b66f5284222d9b0a23925f237101b57569024
John Ezra <john@null.ltd>
2026-08-21 22:52:12 -0700

    Add Unikorn support

--- keyboards/zeix/singa/kohaku/keymaps/ezra/keymap.c
+++ keyboards/zeix/singa/kohaku/keymaps/ezra/keymap.c
@@ -15,18 +15,18 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT_all(
-        QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NO,   KC_BSPC, KC_DEL,
+        QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, KC_BSPC, KC_DEL,
         FN_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, FN_BSLS, KC_PGUP,
         SUP_ESC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, KC_NO,   SUP_ENT, KC_PGDN,
         KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   MO(_FN),
-        KC_LCTL, KC_LGUI, KC_LALT,          KC_NO,            KC_SPC,           KC_NO,            KC_NO,   KC_RALT, CTL_LFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,          _______,          KC_SPC,           _______,          _______, KC_RALT, CTL_LFT, KC_DOWN, KC_RGHT
     ),
 
     [_FN] = LAYOUT_all(
         QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,   _______, KC_INS,
         UG_TOGG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME,
-        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_NO,   _______, KC_END,
-        _______, KC_NO,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______,          KC_NO,            KC_LCTL,          KC_NO,            KC_NO,   _______, _______, _______, _______
+        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______, KC_END,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______,          _______,          KC_LCTL,          _______,          _______, _______, _______, _______, _______
     ),
 };
