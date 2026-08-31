ce0b07734f52694cf0b029738db9b1ad7f1d5f17
John Ezra <john@null.ltd>
2026-08-30 17:00:07 -0700

    Update Dolphin keymap

--- keyboards/linworks/fave87/keymaps/ezra/keymap.c
+++ keyboards/linworks/fave87/keymaps/ezra/keymap.c
@@ -23,15 +23,17 @@ enum layers {
 
 #define SUP_ESC LGUI_T(KC_ESC)
 #define SUP_ENT RGUI_T(KC_ENT)
+#define FN_TAB LT(_FN, KC_TAB)
+#define FN_BSLS LT(_FN, KC_BSLS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT_all(
         KC_ESC,             KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    KC_PSCR, KC_SCRL, KC_PAUS,
-        KC_GRV,   KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,          KC_INS,  KC_HOME, KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSPC,                   KC_DEL,  KC_END,  KC_PGDN,
+        KC_GRV,   KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_GRV,           KC_INS,  KC_HOME, KC_PGUP,
+        FN_TAB,   KC_Q,     KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, FN_BSLS,                   KC_DEL,  KC_END,  KC_PGDN,
         SUP_ESC,  KC_A,     KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, KC_NUHS, SUP_ENT,
         KC_LSFT,  _______,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RALT,                            KC_UP,
-        KC_LCTL,  KC_LALT,  MO(_FN),                            KC_SPC,                                      _______, MO(_FN), KC_RALT, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL,  KC_LGUI,  KC_LALT,                            KC_SPC,                             _______, KC_RALT, KC_RGUI, KC_RCTL,                   KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_FN] = LAYOUT_all(
