f270257eaa892cd3468ab3dad3cee5bec3d84b94
John Ezra <john@null.ltd>
2026-09-15 12:03:53 -0700

    Add Merisi Korsa Mini support

---
+++ keyboards/hineybush/h60/keymaps/ezra/keymap.c
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Merisi Korsa Mini: WKL with split Backspace and split Right Shift.
+
+#include QMK_KEYBOARD_H
+
+enum layers {
+    _COLEMAK,
+    _FN,
+};
+
+#define SUP_ESC LGUI_T(KC_ESC)
+#define SUP_ENT RGUI_T(KC_ENT)
+#define FN_TAB LT(_FN, KC_TAB)
+#define FN_BSPC LT(_FN, KC_BSPC)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_COLEMAK] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
+        QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
+        FN_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, FN_BSPC,
+        SUP_ESC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,          SUP_ENT,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_FN),
+        KC_LCTL, KC_NO,   KC_LALT,                            KC_SPC,                             KC_RALT, KC_NO,   KC_RCTL
+    ),
+
+    [_FN] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
+        QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
+        UG_TOGG, _______, KC_PGDN, KC_PGUP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,          _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_NO,   _______,                            KC_LCTL,                            _______, KC_NO,   _______
+    ),
+};
